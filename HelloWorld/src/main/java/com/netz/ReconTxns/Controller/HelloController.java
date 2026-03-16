76c67919c3a1d8f6321f18bd2df047de9601816e
kumar <kumar@kumar>
2026-03-16 13:28:01 +0530

    First Commit

---
+++ HelloWorld/src/main/java/com/netz/ReconTxns/Controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.netz.ReconTxns.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/world")
+    public String world() {
+        return "Class level mapping!";
+    }
+}
